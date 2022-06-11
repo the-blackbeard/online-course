a26246b57a737b94d5af3b64d899c4808b40032c
Garvit Verma <garvitverma@Garvits-MacBook-Pro.local>
2022-06-11 17:09:58 +0200

    Model validation

--- app/models/course_talent_association.rb
+++ app/models/course_talent_association.rb
@@ -1,2 +1,4 @@
 class CourseTalentAssociation < ApplicationRecord
+  belongs_to :talent, class_name: 'User'
+  belongs_to :course
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :taken_courses, class_name: 'Course', through: :course_talent_association
+  has_many :courses
+
+  validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email
+  validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
