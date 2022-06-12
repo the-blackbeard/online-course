41b85fe655159c42b3dcea5ac41cdbca53c97df9
Garvit Verma <garvitverma@Garvits-MacBook-Pro.local>
2022-06-12 16:09:22 +0200

    Validation for creation of author for a course

--- app/models/course_talent_association.rb
+++ app/models/course_talent_association.rb
@@ -6,4 +6,13 @@ class CourseTalentAssociation < ApplicationRecord
 
   scope :author_type, -> { where(user_type: CourseTalentAssociation.user_types[:author]) }
   scope :talent_type, -> { where(user_type: CourseTalentAssociation.user_types[:talent]) }
+
+  validate :single_author_for_one_course
+
+  def single_author_for_one_course
+    if course.author.present?
+      errors.add(:course, "alreay has an author")
+    end
+
+  end
 end
